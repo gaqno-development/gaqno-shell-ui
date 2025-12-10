8bb44f0c517cc938306017245609945142e10824
Gabriel Aquino <gabriel.deaquino-ext@ab-inbev.com>
2025-12-10 18:46:11 -0300

    Update package-lock.json with new dependencies and configurations; add PostCSS configuration file and update Tailwind CSS paths

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,8 @@ import sharedConfig from "@gaqno-dev/config/tailwind";
 const config: Config = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "../../packages/ui/src/**/*.{js,ts,jsx,tsx,mdx}",
+    "../gaqno-ui/src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@gaqno-dev/ui/src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   presets: [sharedConfig],
 };
