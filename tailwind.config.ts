2221975e04508e60209b8e8a601e31574029b9f7
Gabriel Aquino <gabriel.aquino@outlook.com>
2026-02-18 18:15:51 -0300

    fix(shell): remove tailwindcss-animate to fix dropdown background conflict

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
 import type { Config } from "tailwindcss";
 import path from "path";
-import tailwindcssAnimate from "tailwindcss-animate";
 
 const config: Config = {
   content: [
@@ -11,7 +10,6 @@ const config: Config = {
     path.resolve(__dirname, "../@gaqno-frontcore/src/**/*.{js,ts,jsx,tsx}"),
     path.resolve(__dirname, "./node_modules/@gaqno-development/frontcore/**/*.{js,ts,jsx,tsx}"),
   ],
-  plugins: [tailwindcssAnimate],
 };
 
 export default config;
